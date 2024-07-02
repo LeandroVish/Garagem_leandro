15e2a3e6de9831ce9dfa5cf13639e8c6e141c396
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-07-02 08:15:01 -0300

    Correção em marca.py

--- core/models/marca.py
+++ core/models/marca.py
@@ -5,4 +5,4 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length=50, null=True)
 
     def __str__(self):
-        return f"{self.nome.ToUpperCase()} {self.id}"
+        return f"{self.nome.upper()} {self.id}"
