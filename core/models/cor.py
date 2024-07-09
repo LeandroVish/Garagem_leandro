7f36ed31c5e62b1747e409bbacd8240068aebf9c
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-07-09 08:23:01 -0300

    correção de nomes

--- core/models/cor.py
+++ core/models/cor.py
@@ -5,3 +5,7 @@ class Cor(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.id})"
+    
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
